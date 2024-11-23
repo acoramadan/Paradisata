87342f22e07854ab2681363194b053fecdc9d279
acoramadan <laxmiwatimaani@gmail.com>
2024-11-23 20:31:13 +0800

    feat: adding several activites, and fixing some adapters

---
+++ app/src/main/java/com/muflidevs/paradisata/ui/view/TourGuideIdentityAuthActivity.kt
@@ -0,0 +1,21 @@
+package com.muflidevs.paradisata.ui.view
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.muflidevs.paradisata.R
+
+class TourGuideIdentityAuthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_tour_guide_identity_auth)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
