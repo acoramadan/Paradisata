1923bfdd7817003f71d5bdc039c035cc289aed13
acoramadan <laxmiwatimaani@gmail.com>
2024-12-06 08:47:26 +0800

    Feat: adding prosess booking, completed booking, success registration, failed layout

--- app/src/main/java/com/muflidevs/paradisata/ui/view/ProsesRegisterActivity.kt
+++ app/src/main/java/com/muflidevs/paradisata/ui/view/ProsesRegisterActivity.kt
@@ -7,11 +7,11 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.muflidevs.paradisata.R
 
-class PackageInsertActivity : AppCompatActivity() {
+class ProsesRegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_package_insert)
+        setContentView(R.layout.activity_proses_register)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
--- app/src/main/java/com/muflidevs/paradisata/ui/view/tourguide/PackageInsertActivity.kt
+++ app/src/main/java/com/muflidevs/paradisata/ui/view/tourguide/PackageInsertActivity.kt
@@ -1,4 +1,4 @@
-package com.muflidevs.paradisata.ui.view
+package com.muflidevs.paradisata.ui.view.tourguide
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
@@ -7,11 +7,11 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.muflidevs.paradisata.R
 
-class ActivityTransportationType : AppCompatActivity() {
+class PackageInsertActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_transportation_type)
+        setContentView(R.layout.activity_package_insert)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
